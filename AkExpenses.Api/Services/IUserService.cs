c4cd133d09352cc0ded0056bad8f0e8c87c7da18
Semos5245 <s.alan.abd1998@gmail.com>
2019-09-10 14:09:23 +0300

    Fixes

--- AkExpenses.Api/Services/IUserService.cs
+++ AkExpenses.Api/Services/IUserService.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using v = AKSoftware.Text; 
 namespace AkExpenses.Api.Services
 {
     public interface IUserService
