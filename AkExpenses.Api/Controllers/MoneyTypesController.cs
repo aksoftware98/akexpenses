8ac7c61c23f9fc1d8968e0519f00d5d185d48fb5
Semos5245 <s.alan.abd1998@gmail.com>
2019-09-10 14:57:14 +0300

    Initialize MoneyTypes and Outcomes Controllers

---
+++ AkExpenses.Api/Controllers/MoneyTypesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AkExpenses.Api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkExpenses.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoneyTypesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public MoneyTypesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        #region Get
+
+        
+
+        #endregion
+
+        #region Post
+
+
+
+        #endregion
+
+        #region Put
+
+        
+        
+        #endregion
+
+        #region Delete
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string id)
+        {
+            //Validate the id
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest();
+            }
+
+            //Get Money type
+            var type = await context.MoneyTypes.FindAsync(id);
+
+            if (type == null)
+            {
+                return NotFound();
+            }
+
+            context.MoneyTypes.Remove(type);
+            await context.SaveChangesAsync();
+
+            return Ok(new
+            {
+
+            });
+        }
+        
+        #endregion
+    }
+}
