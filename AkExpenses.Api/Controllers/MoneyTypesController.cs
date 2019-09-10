70770f03056fd1567b7d75f48893b58480f46041
Semos5245 <s.alan.abd1998@gmail.com>
2019-09-10 15:46:51 +0300

    MoneyTypes Get,Post,Delete

--- AkExpenses.Api/Controllers/MoneyTypesController.cs
+++ AkExpenses.Api/Controllers/MoneyTypesController.cs
@@ -3,38 +3,124 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AkExpenses.Api.Data;
+using AkExpenses.Models;
+using AkExpenses.Models.Shared.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AkExpenses.Api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class MoneyTypesController : ControllerBase
     {
-        private readonly ApplicationDbContext context;
+        private readonly ApplicationDbContext db;
 
         public MoneyTypesController(ApplicationDbContext context)
         {
-            this.context = context;
+            this.db = context;
         }
+
+
 
         #region Get
 
-        
+        /// <summary>
+        /// Gets all money types in the database
+        /// </summary>
+        /// <returns></returns>
+        [Route("Get")]
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            //Get logged in account
+            var account = await GetAccount();
+
+            //Get money types
+            var moneyTypes = db.MoneyTypes.Where(t => t.AccountId == account.Id).OrderByDescending(t => t.Name);
+
+            //Create response
+            var response = new AkExpenses.Models.Shared.HttpResponse<IEnumerable<MoneyType>>
+            {
+                Count = moneyTypes.Count(),
+                IsSuccess = true,
+                Message = "Money types have been retrieved successfully.",
+                Value = moneyTypes
+            };
+
+            //Return result
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Gets a specific money type
+        /// </summary>
+        /// <param name="id">Id of the money type to get</param>
+        /// <returns></returns>
+        [Route("Get/{id}")]
+        [HttpGet]
+        public IActionResult Get(string id)
+        {
+            //Validate the id
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest(new AkExpenses.Models.Shared.HttpResponse<object>
+                {
+                    IsSuccess = false,
+                    Message = "Id sent can't be empty."
+                });
+            }
+
+            //Get the account
+            var type = db.MoneyTypes.Find(id);
+
+            if (type == null)
+            {
+                return NotFound(new AkExpenses.Models.Shared.HttpResponse<object>
+                {
+                    IsSuccess = false,
+                    Message = "Money Type does not exist."
+                });
+            }
+
+            return Ok(new AkExpenses.Models.Shared.HttpResponse<MoneyType>
+            {
+                Count = 1,
+                IsSuccess = true,
+                Message = $"Money type has been retrieved successfully.",
+                Value = type
+            });
+        }
 
         #endregion
 
         #region Post
 
+        [Route("Post")]
+        [HttpPost]
+        public async Task<IActionResult> Post(MoneyTypeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Get the account
+                
+            }
 
+            return BadRequest(new AkExpenses.Models.Shared.HttpResponse<object>
+            {
+                IsSuccess = false,
+                Message = "Model sent has some errors."
+            });
+        }
 
         #endregion
 
         #region Put
 
-        
-        
+
+
         #endregion
 
         #region Delete
@@ -49,22 +135,32 @@ namespace AkExpenses.Api.Controllers
             }
 
             //Get Money type
-            var type = await context.MoneyTypes.FindAsync(id);
+            var type = await db.MoneyTypes.FindAsync(id);
 
             if (type == null)
             {
                 return NotFound();
             }
 
-            context.MoneyTypes.Remove(type);
-            await context.SaveChangesAsync();
+            db.MoneyTypes.Remove(type);
+            await db.SaveChangesAsync();
 
             return Ok(new
             {
 
             });
         }
-        
+
+        #endregion
+
+        #region Helper Functions
+
+        private async Task<Account> GetAccount()
+        {
+            string id = User.Claims.SingleOrDefault(s => s.Type == "AccountId").Value;
+            return await db.Accounts.FindAsync(id);
+        }
+
         #endregion
     }
 }
---
+++ AkExpenses.Models/Shared/ViewModels/MoneyTypeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AkExpenses.Models.Shared.ViewModels
+{
+    public class MoneyTypeViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(265)]
+        public string Desription { get; set; }
+
+        [Required]
+        public string AccountId { get; set; }
+
+    }
+}
